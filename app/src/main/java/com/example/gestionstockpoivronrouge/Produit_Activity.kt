ff2b2e1a940f66748a5d0dcede66dcbcad7c9961
Napoleon Wagnerson <107638402+papi-DarknesHt@users.noreply.github.com>
2025-03-28 08:02:16 -0400

    Update Produit_Activity.kt

--- app/src/main/java/com/example/gestionstockpoivronrouge/Produit_Activity.kt
+++ app/src/main/java/com/example/gestionstockpoivronrouge/Produit_Activity.kt
@@ -7,18 +7,16 @@ import android.widget.Button
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gestionstockpoivronrouge.database.AppDatabase
-import com.example.gestionstockpoivronrouge.repository.CompteRepository
 import com.example.gestionstockpoivronrouge.repository.ProduitRepository
-import com.example.gestionstockpoivronrouge.viewmodel.CompteViewModel
 import com.example.gestionstockpoivronrouge.viewmodel.ProduitViewModel
 
 class Produit_Activity : AppCompatActivity() {
 
     private lateinit var gestionProduit_Activity: Produit_Activity
     private val produitViewModel: ProduitViewModel by viewModels {
-        val daoCompte = AppDatabase.getDatabase(this).compteDao()
-        val repository = CompteRepository(daoCompte)
-        produitViewModel.FactoryProduit(repository) // Utilisation correcte de la Factory interne
+        val daoProduit = AppDatabase.getDatabase(this).produitDao()
+        val repository = ProduitRepository(daoProduit)
+        ProduitViewModel.FactoryProduit(repository) // Utilisation correcte de la Factory interne
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
