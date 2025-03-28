6875f454432b7ddfa3ca636a8b8dbd0451170f3d
Napoleon Wagnerson <107638402+papi-DarknesHt@users.noreply.github.com>
2025-03-28 16:03:16 -0400

    gestion produit ajout et affichage

--- app/src/main/java/com/example/gestionstockpoivronrouge/ProduitAdapter.kt
+++ app/src/main/java/com/example/gestionstockpoivronrouge/ProduitAdapter.kt
@@ -6,44 +6,42 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
-import com.example.gestionstockpoivronrouge.model.Compte
+import com.example.gestionstockpoivronrouge.model.Produit
 
-class GestionAdapter(
+class ProduitAdapter(
     private val context: Context,
-    private var comptes: MutableList<Compte>,  // Liste des comptes à afficher
-    private val onEditClick: (Compte) -> Unit,  // Callback pour l'édition
-    private val onDeleteClick: (Compte) -> Unit // Callback pour la suppression
-) : RecyclerView.Adapter<GestionAdapter.CompteViewHolder>() {
+    private var produits: MutableList<Produit>,  // Liste des comptes à afficher
+    private val onEditClick: (Produit) -> Unit,  // Callback pour l'édition
+    private val onDeleteClick: (Produit) -> Unit // Callback pour la suppression
+) : RecyclerView.Adapter<ProduitAdapter.ProduitViewHolder>() {
 
     private var selectedPosition = -1  // Pour savoir quel compte est sélectionné
 
     // Méthode pour mettre à jour la liste des comptes
-    fun setComptes(newCompte: List<Compte>) {
-        comptes.clear()
-        comptes.addAll(newCompte)  // Ajout des nouveaux comptes à la liste
+    fun setComptes(newProduit: List<Produit>) {
+        produits.clear()
+        produits.addAll(newProduit)  // Ajout des nouveaux comptes à la liste
         notifyDataSetChanged()  // Notifie l'adaptateur que les données ont changé
     }
 
     // ViewHolder pour chaque élément dans le RecyclerView
-    inner class CompteViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val nom: TextView = view.findViewById(R.id.nomCompte)
-        val prenom: TextView = view.findViewById(R.id.prenomCompte)
-        val email: TextView = view.findViewById(R.id.emailCompte)
-        val statut: TextView = view.findViewById(R.id.statuCompte)
-        val btnEdit: ImageView = view.findViewById(R.id.btnEdit)
-        val btnDelete: ImageView = view.findViewById(R.id.btnDelete)
+    inner class ProduitViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val nomProduit: TextView = view.findViewById(R.id.nomproduit)
+        val codeProduit: TextView = view.findViewById(R.id.codeProduit)
+        val categorieProduit: TextView = view.findViewById(R.id.categorieprod)
+        val btnEdit: ImageView = view.findViewById(R.id.btnEditproduit)
+        val btnDelete: ImageView = view.findViewById(R.id.btnDeleteproduit)
 
         init {
             // Gérer l'édition et la suppression avec les callbacks
             btnEdit.setOnClickListener {
-                val compte = comptes[adapterPosition]
+                val compte = produits[adapterPosition]
                 onEditClick(compte) // Appel du callback d'édition
             }
 
             btnDelete.setOnClickListener {
-                val compte = comptes[adapterPosition]
+                val compte = produits[adapterPosition]
                 onDeleteClick(compte) // Appel du callback de suppression
             }
 
@@ -63,10 +61,10 @@ class GestionAdapter(
     }
 
     // Créer un ViewHolder pour chaque item
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CompteViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProduitViewHolder {
         val view = LayoutInflater.from(context)
-            .inflate(R.layout.item_gestion_compte, parent, false)
-        return CompteViewHolder(view)
+            .inflate(R.layout.item_gestion_produit, parent, false)
+        return ProduitViewHolder(view)
     }
 
     // Lier les données du compte avec les vues de l'item
@@ -84,35 +82,18 @@ class GestionAdapter(
         holder.btnDelete.visibility = if (position == selectedPosition) View.VISIBLE else View.GONE
     }*/
 
-    override fun onBindViewHolder(holder: CompteViewHolder, position: Int) {
-        val compte = comptes[position]
+    override fun onBindViewHolder(holder: ProduitViewHolder, position: Int) {
+        val produit = produits[position]
 
-        holder.nom.text = compte.nom
-        holder.prenom.text = compte.prenom
-        holder.email.text = compte.email
-        holder.statut.text = compte.statut
-        // ... autres bind ...
-
+        holder.nomProduit.text = produit.nom
+        holder.categorieProduit.text = produit.categorie
+        holder.codeProduit.text = produit.code
         // Afficher ou cacher les icônes en fonction de la sélection
         holder.btnEdit.visibility = if (position == selectedPosition) View.VISIBLE else View.GONE
         holder.btnDelete.visibility = if (position == selectedPosition) View.VISIBLE else View.GONE
     }
 
 
-    /*@Override
-    fun onBindViewHolder(holder: CompteViewHolder, position: Int) {
-        val compte = comptes[position]
-
-        holder.nom.text = compte.nom
-        holder.prenom.text = compte.prenom
-        holder.email.text = compte.email
-        holder.statut.text = compte.statut
-
-        // Afficher ou masquer les icônes Modifier et Supprimer
-        holder.btnEdit.visibility = if (position == selectedPosition) View.VISIBLE else View.GONE
-        holder.btnDelete.visibility = if (position == selectedPosition) View.VISIBLE else View.GONE
-    }*/
-
     // Retourner la taille de la liste des comptes
-    override fun getItemCount(): Int = comptes.size
+    override fun getItemCount(): Int = produits.size
 }
--- app/src/main/java/com/example/gestionstockpoivronrouge/model/Produit.kt
+++ app/src/main/java/com/example/gestionstockpoivronrouge/model/Produit.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "produit",indices = [androidx.room.Index(value = ["code"], unique = true)])
 data class Produit(
     @PrimaryKey(autoGenerate = true)
-    val id : Int,
+    val id : Int=0,
     val code: String ,
     val nom: String,
     val categorie: String,
