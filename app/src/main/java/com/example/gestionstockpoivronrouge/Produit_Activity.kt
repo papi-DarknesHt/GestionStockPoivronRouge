1602a51c87a1caf99ad70f4801b543cbde8be9d5
Napoleon Wagnerson <107638402+papi-DarknesHt@users.noreply.github.com>
2025-03-28 07:52:10 -0400

    version8

--- app/src/main/java/com/example/gestionstockpoivronrouge/Produit_Activity.kt
+++ app/src/main/java/com/example/gestionstockpoivronrouge/Produit_Activity.kt
@@ -4,9 +4,23 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.example.gestionstockpoivronrouge.database.AppDatabase
+import com.example.gestionstockpoivronrouge.repository.CompteRepository
+import com.example.gestionstockpoivronrouge.repository.ProduitRepository
+import com.example.gestionstockpoivronrouge.viewmodel.CompteViewModel
+import com.example.gestionstockpoivronrouge.viewmodel.ProduitViewModel
 
 class Produit_Activity : AppCompatActivity() {
+
+    private lateinit var gestionProduit_Activity: Produit_Activity
+    private val produitViewModel: ProduitViewModel by viewModels {
+        val daoCompte = AppDatabase.getDatabase(this).compteDao()
+        val repository = CompteRepository(daoCompte)
+        produitViewModel.FactoryProduit(repository) // Utilisation correcte de la Factory interne
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activityproduit)
--- app/src/main/java/com/example/gestionstockpoivronrouge/viewmodel/ProduitViewModel.kt
+++ app/src/main/java/com/example/gestionstockpoivronrouge/viewmodel/ProduitViewModel.kt
@@ -1,12 +1,7 @@
 package com.example.gestionstockpoivronrouge.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
-import com.example.gestionstockpoivronrouge.model.Compte
+import androidx.lifecycle.*
 import com.example.gestionstockpoivronrouge.model.Produit
-import com.example.gestionstockpoivronrouge.repository.CompteRepository
 import com.example.gestionstockpoivronrouge.repository.ProduitRepository
 import kotlinx.coroutines.launch
 
@@ -36,14 +31,15 @@ class ProduitViewModel(private val repository: ProduitRepository) : ViewModel() 
             onResult(true, "Produit supprimé")
         }
     }
-}
 
-class Factory(private val repository: ProduitRepository) : ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(CompteViewModel::class.java)) {
-            @Suppress("UNCHECKED_CAST")
-            return ProduitViewModel(repository) as T
+
+    class FactoryProduit(private val repository: ProduitRepository) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(ProduitViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return ProduitViewModel(repository) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModel class")
         }
-        throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
